c7d96208ad272f9820dc81570101021c08cba675
Hugo Costa <costarafaelhugo@gmail.com>
2021-08-17 14:20:18 -0300

    Create the logic to make and run de dices

---
+++ app/src/main/java/com/hugorafaelcosta/d20app/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.hugorafaelcosta.d20app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myFirstDice = Dice(6)
+        println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}.")
+
+        val mySecondDice = Dice(20)
+        println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}.")
+
+    }
+
+    class Dice (val numSides:Int){
+
+        fun roll():Int{
+            return  (1..numSides).random()
+        }
+    }
+}
