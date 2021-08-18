2c8275082aaedbdb1065d99cb639b63a2472ead0
Hugo Costa <costarafaelhugo@gmail.com>
2021-08-18 13:24:02 -0300

    Add the logicallu for the d20

--- app/src/main/java/com/hugorafaelcosta/d20app/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/d20app/MainActivity.kt
@@ -1,25 +1,33 @@
 package com.hugorafaelcosta.d20app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myFirstDice = Dice(6)
-        println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}.")
+        val rollButton: Button = findViewById(R.id.button)
 
-        val mySecondDice = Dice(20)
-        println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}.")
+        rollButton.setOnClickListener { rollDice() }
 
     }
 
-    class Dice (val numSides:Int){
+    private fun rollDice() {
+        val dice = Dice(20)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.result)
+        resultTextView.text = diceRoll.toString()
+    }
+}
 
-        fun roll():Int{
-            return  (1..numSides).random()
-        }
+
+private class Dice(val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
